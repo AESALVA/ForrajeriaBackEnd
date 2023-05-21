977251a0428a4d5edb3860f05cca126e0fae0dd4
AESalva <eduardo_salva@hotmail.com>
2023-05-21 20:10:11 -0300

    add route users

---
+++ Routes/users.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const User = require("../Models/user");
+const bcrypt = require("bcryptjs");
+const { body, validationResult } = require("express-validator");
+const nodemailer = require("nodemailer");
+
+
+router
+  .get("/all", async (req, res) => {
+    console.log("GET /users/all");
+    let Users = [];
+    try {
+      const allUsers = await User.find();
+      allUsers.map((users) => {
+        user = { name: users.name, role: users.role, mail:users.mail };
+        Users = [...Users, user];
+      });
+      res.status(200).json(Users);
+    } catch (error) {
+      res.status(400).json({ error: true, message: error });
+    }
+})
+
+module.exports = router;
--- index.js
+++ index.js
@@ -7,8 +7,15 @@ require('dotenv').config();
 let cors = require('cors')
 app.use(cors({origin:"*"}));
 
+mongoose.connect(process.env.URI, {
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+  }).then(()=>{console.log('database connection OK')}).catch((error)=>{console.error(error)});
 
+const PORT = process.env.PORT || 8000;
 
+const usersRoutes = require('./Routes/users');
+app.use('/users', usersRoutes)
 
 
 app.listen(PORT, ()=>{console.log('server running on port '+ PORT )});
